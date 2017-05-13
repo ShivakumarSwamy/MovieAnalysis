8bc21069a4f6cef2dc142647d2fb2201efc48ad2
GeethanjaliNivas <anjali.ckec@gmail.com>
2017-05-13 22:33:32 +0200

    WebCrawl
    web crawl module (imdb and tmdb)

--- webcrawl.py
+++ webcrawl.py
@@ -20,3 +20,29 @@ class WebCrawl:
             print ('URL Error')
         
         return boxOfficeResultIMDB
+ 
+#tmdb
+import urllib.request
+from bs4 import BeautifulSoup
+ 
+class WebCrawl:
+    def extractBoxOfficeByTMDB(tmdbid):
+        boxOfficeResultTMDB = 'N/A'
+        try:
+            urlTMDB = urllib.request.urlopen('https://www.themoviedb.org/movie/14784-the-fall' + str('tmdbid')).read()      
+#soup = BeautifulSoup(url)
+            soup = BeautifulSoup(urlTMDB,'lxml')
+            for i in soup.find_all('section', class_='facts left_column'):
+                for p_tag in soup.find_all('p'):
+                    if 'Revenue' in p_tag.text:
+                        rev = p_tag.text
+                        print(rev)   
+                    for x in rev.split()[1].split('$')[1].split(','):
+                        boxofficeTMDB += x
+                        boxOfficeResulTMDB = boxofficeTMDB
+        except requests.exceptions.RequestException:
+            print ('URL Error')
+        
+        return boxOfficeResultTMDB
+                        
+            
